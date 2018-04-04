0e24e0c1f3a2c36950fd46328be21a6bb882ea1b
Bartosz Suchocki <suchar1270@wp.pl>
2018-04-04 11:23:54 +0200

    dodaje zalegly plik naglowkowy, powinien byc w poprzednim commicie

--- Simulator/src/simulator.h
+++ Simulator/src/simulator.h
@@ -1,10 +1,12 @@
 #ifndef _SIMULATOR_
 #define _SIMULATOR_
 #include <stdio.h>
+#include <stdlib.h>
 #include "board.h"
+int read_rules(FILE *in, int rules[],int *neighbourhood);//w specyfikacji ta funkcja byla w module main. Tu zwraca 0 przy powodzeniu lub 1 przy niepowodzeniu;
 board_t simulate_generation(board_t previous_generation, int rules[], int neighbourhood);
 int count_neighbour_alive(int x, int y, int neighbourhood, board_t b); // zmiana w stos do specyfikacji: przyjmuje dodatkowo plansze b
-int what_happens_with_cell(int current_state,int neighbours_alive, int rules[]); // zmiana w stosunku do specyfikacji: i dostaje dodatkowo current_state oraz zwraca stan komorki w nastepnej generacji (0 lub 1)
+char what_happens_with_cell(int current_state,int neighbours_alive, int rules[]); // zmiana w stosunku do specyfikacji: i dostaje dodatkowo current_state oraz zwraca stan komorki w nastepnej generacji (0 lub 1)
 
 
 
